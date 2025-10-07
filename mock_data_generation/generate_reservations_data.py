0c81eedae4abafed871547d178daf2fe3e1df92c
Javier Delgado Noriega <javierdn@Javiers-MacBook-Pro.local>
2025-10-07 16:20:32 -0700

    add duration dates in random records

--- mock_data_generation/generate_reservations_data.py
+++ mock_data_generation/generate_reservations_data.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import random
 import boto3
 
@@ -9,13 +9,29 @@ def generate_mock_reservation_data(num_records):
 
     records = []
     for i in range(num_records):
+        
+        start_date_min = datetime(2025, 1, 1)
+        start_date_max = datetime(2027, 12, 1)
+        # Generate a random start date
+        days_between = (start_date_max - start_date_min).days
+        random_start_date = start_date_min + timedelta(days=random.randint(0, days_between))
+
+        # Generate a random duration between 1 and 30 days
+        duration_days = random.randint(1, 30)
+        random_end_date = random_start_date + timedelta(days=duration_days)
+
+        # Converto dates to string format YYYY-MM-DD
+        start_date_str = random_start_date.strftime("%Y-%m-%d")
+        end_date_str = random_end_date.strftime("%Y-%m-%d")
+
         record = {
             "bookingId": f"{datetime.now().strftime('%Y%m%d%H%M%S')}-{i}",
             "userId": random.randint(1, 1000),
             "propertyId": f"property-{i%200}", 
             "location": f"City-{i%5}, Country-{i%3}",  
-            "startDate": f"2025-01-{(i%28)+1:02d}",
-            "endDate": f"2027-12-{(i%28)+2:02d}",
+            "startDate": start_date_str,
+            "endDate": end_date_str,
+            "reservationDurationDays": duration_days,
             "price": round(50 + (i * 3.75) % 450, 2)  
             }
         records.append(record)
